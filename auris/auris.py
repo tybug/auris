4d2c2faa2ad695ff3cd11515edbc8bcd8e4e451e
Liam DeVoe <orionldevoe@gmail.com>
2024-10-15 17:22:10 -0400

    rewrite using pyaudio

--- auris/auris.py
+++ auris/auris.py
@@ -1,65 +1,116 @@
-from enum import Enum
-from time import perf_counter
+import threading
+from pathlib import Path
 
+import pyaudio
+from pyaudio import PyAudio
+import numpy as np
 from pydub import AudioSegment
-from pydub.playback import _play_with_simpleaudio
 
-class Side(Enum):
-    LEFT = "left"
-    RIGHT = "right"
 
-    @classmethod
-    def _missing_(cls, value):
-        if value == "l":
-            return Side.LEFT
-        if value == "r":
-            return Side.RIGHT
-        return super()._missing_(value)
+class Audio:
+    def __init__(self, path: Path, mod):
+        self.track = 0
+        self.audio = AudioSegment.from_file(path)
+        self.mod = mod
 
-    def opposite(self):
-        return Side.LEFT if self is Side.RIGHT else Side.RIGHT
+        self._pyaudio = PyAudio()
+        self._pyaudio_stream = None
+
+        self.increment_track_flag = threading.Event()
+
+    def prepare_audio(self):
+        samples = np.array(self.audio.get_array_of_samples())
+
+        if self.audio.channels == 2:
+            left = samples[::2]
+            right = samples[1::2]
+        else:
+            # mono audio (probably?)
+            left = samples
+            right = samples
+
+        return (
+            np.column_stack((left, right)).astype(np.float32)
+            / np.iinfo(samples.dtype).max
+        )
+
+    def audio_callback(self, in_data, frame_count, time_info, status):
+        if self.increment_track_flag.is_set():
+            self.track = (self.track + 1) % 2
+            self.increment_track_flag.clear()
+
+        chunk = self.stereo[:frame_count]
+        self.stereo = self.stereo[frame_count:]
+
+        if len(chunk) < frame_count:
+            # replay from beginning
+            self.stereo = self.original_stereo.copy()
+            chunk = np.pad(
+                chunk, ((0, frame_count - len(chunk)), (0, 0)), mode="constant"
+            )
+
+        chunk = self.mod(chunk.copy(), track=self.track)
+        return (chunk.tobytes(), pyaudio.paContinue)
+
+    def start_playback(self):
+        self.original_stereo = self.prepare_audio()
+        self.stereo = self.original_stereo.copy()
+
+        pyaudio_stream = self._pyaudio.open(
+            format=pyaudio.paFloat32,
+            channels=2,
+            rate=self.audio.frame_rate,
+            output=True,
+            stream_callback=self.audio_callback,
+        )
+
+        pyaudio_stream.start_stream()
+
+    def stop(self):
+        if self._pyaudio_stream is not None:
+            self._pyaudio_stream.stop_stream()
+            self._pyaudio_stream.close()
+            self._pyaudio_stream = None
+        self._pyaudio.terminate()
+
+    def run(self):
+        self.start_playback()
+        try:
+            while True:
+                input("press enter to increment track")
+                self.increment_track_flag.set()
+        finally:
+            self.stop()
+
 
 class Auris:
-    def __init__(self, song_path):
-        self.song = AudioSegment.from_mp3(song_path)
+    def __init__(self, path):
+        def lr(chunk, *, track):
+            chunk[:, track] = 0
+            return chunk
 
-        # convert to mono, we'll play this on only the left or only the right
-        # channels later
-        mono_song = self.song. set_channels(1)
-        silent = AudioSegment.silent(
-            duration=self.song.duration_seconds * 1000,
-            frame_rate=self.song.frame_rate
-        )
-        self.left_stereo = AudioSegment.from_mono_audiosegments(
-            mono_song, silent)
-        self.right_stereo = AudioSegment.from_mono_audiosegments(
-            silent, mono_song)
+        def pan(chunk, *, track):
+            if track == 0:
+                return chunk
 
-    def play(self, side, *, ms=0):
-        side = Side(side)
-        song = self.left_stereo if side is Side.LEFT else self.right_stereo
-        song = song[ms:]
-        # simpleaudio handles threading for us and allows us to stop playback at
-        # any time, which is why I chose it
-        return _play_with_simpleaudio(song)
+            pan = -0.1
+            chunk[:, 0] *= 1 - max(0, pan)
+            chunk[:, 1] *= 1 + min(0, pan)
+            return chunk
 
-    def alternate_sides(self):
-        # default to right side first, no particular reason why
-        side = Side.RIGHT
-        # track elapsed time so we can seamlessly switch between left and right
-        # segments
-        start = perf_counter()
-        while True:
-            # perf_counter is in seconds, convert to ms
-            elapsed = (perf_counter() - start) * 1000
-            playback = self.play(side, ms=elapsed)
-            input("press enter to switch sides ")
-            playback.stop()
-            side = side.opposite()
+        self.audio = Audio(path, mod=lr)
+
+    def run(self):
+        self.audio.run()
+
+    def run_tournament(self):
+        min_pan = -1
+        max_pan = 1
+
 
 song_path = input("enter song path: ")
 # drag and dropped files include leading and trailing quotes in their path in
 # the vscode terminal, so strip if present
 song_path = song_path.strip("'")
 auris = Auris(song_path)
-auris.alternate_sides()
+auris.run()
